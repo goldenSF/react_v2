8eb237f879144ed10535f87ac9301855f9c54e5a
Kaung Win <tkong7511@yahoo.com>
2020-12-13 00:09:22 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -30,7 +30,7 @@ function FormatDate(date) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
     return(
         <div className="col-md-5 and m-1">
@@ -44,7 +44,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     </div> 
                 );
             })}
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+            <CommentForm campsiteId={campsiteId} postComment={postComment} />
         </div>    
         );
     }
@@ -70,7 +70,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -182,7 +182,7 @@ function CampsiteInfo(props) {
                 <RenderCampsite campsite ={props.campsite} />
                 <RenderComments 
                     comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     campsiteId={props.campsite.id}
                 />
             </div>
